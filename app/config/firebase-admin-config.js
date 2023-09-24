71aa193eb74a8418f27eac3a90e5d4d94dbabb8a
Jesse <bigjmn@gmail.com>
2023-09-24 15:24:36 -0400

    changed key storage

--- app/config/firebase-admin-config.js
+++ app/config/firebase-admin-config.js
@@ -1,7 +1,8 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app'
 import { getFirestore } from "firebase-admin/firestore"
+const serviceAccount = JSON.parse(process.env.FIREBASE_SECRET_KEY);
 const firebaseAdminConfig = {
-    credential: cert(process.env.FIREBASE_SECRET_KEY)
+    credential: cert(serviceAccount)
 }
 
 if (!getApps().length) {
